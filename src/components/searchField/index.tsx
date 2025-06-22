823cfd7401ba4fa8c2859da353d0e3b1c5eb5b75
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2025-06-21 21:18:47 -0300

    :sparkles: create search field component

---
+++ src/components/searchField/index.tsx
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+interface SearchFieldProps {
+  searchValue: string;
+  onSearchValueChange: (value: string) => void;
+}
+
+const SearchField = ({
+  searchValue,
+  onSearchValueChange,
+}: SearchFieldProps) => {
+  return (
+    <input
+      type="text"
+      placeholder="Buscar Alien"
+      onChange={(e) => onSearchValueChange(e.target.value)}
+      value={searchValue}
+      className="input-reset ba bw2 b--green bg-black white f5 pa3 w-30 br1 focus-effect"
+    />
+  );
+};
+
+export default SearchField;

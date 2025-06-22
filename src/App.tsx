ae37a41382556c9741fc9abecdf57949546e69b3
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2025-06-21 21:20:52 -0300

    :sparkles: create main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useState, useMemo } from "react";
 import "./App.css";
+import { SearchField, CardList } from "./components";
+import { AliensData } from "./data/aliensData";
 
 function App() {
+  const [searchValue, onSearchValueChange] = useState<string>("");
+
+  const filteredAliens = useMemo(() => {
+    const lowercasedSearch = searchValue.toLowerCase();
+
+    return AliensData.filter(
+      (alien) =>
+        alien.userName.toLowerCase().includes(lowercasedSearch) ||
+        alien.email.toLowerCase().includes(lowercasedSearch)
+    );
+  }, [searchValue]);
+
   return (
     <div className="tc">
       <h1 id="title">AlienFriends</h1>
+      <SearchField
+        searchValue={searchValue}
+        onSearchValueChange={onSearchValueChange}
+      />
+      <CardList filteredAliens={filteredAliens} />
     </div>
   );
 }

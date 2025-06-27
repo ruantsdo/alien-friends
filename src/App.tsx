fedeb20746481040e0d46923b1e7f43b5aa93102
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2025-06-27 19:09:45 -0300

    ✨ randomizer implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,56 @@
-import { useState, useMemo } from "react";
+// React
+import { useState, useMemo, useEffect } from "react";
+//Styles
 import "./App.css";
+//Components
 import { SearchField, CardList } from "./components";
-import { AliensData } from "./data/aliensData";
+//Types
+import { AlienData } from "./types";
 
 function App() {
   const [searchValue, onSearchValueChange] = useState<string>("");
+  const [isGettingData, setIsGettingData] = useState<boolean>(true);
+  const [aliensData, setAliensData] = useState<AlienData[]>([]);
+  const [screenMessage, setScreenMessage] = useState<string>(
+    "Obtendo informação extra-planetares ..."
+  );
+
+  const getAliensData = async () => {
+    setIsGettingData(true);
+    setScreenMessage("Obtendo informação extra-planetares ...");
+
+    try {
+      const response = await fetch(
+        "https://randomuser.me/api/?inc=name,email,login&results=10&noinfo"
+      );
+      const data = await response.json();
+      setAliensData(data.results);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+      setScreenMessage("Falha ao recuperar os dados dos Aliens ... :(");
+    } finally {
+      setIsGettingData(false);
+    }
+  };
 
   const filteredAliens = useMemo(() => {
     const lowercasedSearch = searchValue.toLowerCase();
 
-    return AliensData.filter(
-      (alien) =>
-        alien.userName.toLowerCase().includes(lowercasedSearch) ||
-        alien.email.toLowerCase().includes(lowercasedSearch)
-    );
-  }, [searchValue]);
+    if (!isGettingData || !aliensData.length) {
+      return aliensData.filter(
+        (alien) =>
+          alien.name.first.toLowerCase().includes(lowercasedSearch) ||
+          alien.name.title.toLowerCase().includes(lowercasedSearch) ||
+          alien.email.toLowerCase().includes(lowercasedSearch)
+      );
+    } else {
+      return [];
+    }
+  }, [searchValue, isGettingData, aliensData]);
+
+  useEffect(() => {
+    getAliensData();
+  }, []);
 
   return (
     <div className="tc">
@@ -23,7 +59,11 @@ function App() {
         searchValue={searchValue}
         onSearchValueChange={onSearchValueChange}
       />
-      <CardList filteredAliens={filteredAliens} />
+      {isGettingData ? (
+        <p>{screenMessage}</p>
+      ) : (
+        <CardList filteredAliens={filteredAliens} />
+      )}
     </div>
   );
 }
